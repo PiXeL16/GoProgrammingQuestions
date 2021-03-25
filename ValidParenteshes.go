5f999db8252265cd5cf402c902ffe79d4993ab64
Chris Jimenez <cjimenez16@gmail.com>
2021-03-25 20:02:17 +0000

    Parentheses problem second try

--- ValidParenteshes.go
+++ ValidParenteshes.go
@@ -15,8 +15,33 @@ func main() {
 
 	s := "{[]}"
 
-	fmt.Print(isValidParentheses(s))
+	fmt.Print(isValid(s))
 
+}
+
+func isValid(s string) bool {
+
+	stack := make([]rune, 0)
+	m := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	for _, c := range s {
+		switch c {
+		case '(', '{', '[':
+			stack = append(stack, c)
+		case ')', '}', ']':
+			if len(stack) == 0 || stack[len(stack)-1] != m[c] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		}
+	}
+
+	return len(stack) == 0
 }
 
 func isValidParentheses(s string) bool {
